d188fa14a129f3c9da92cb13b8591955e893aee9
llallonen <llallonen@gmail.com>
2023-04-20 20:13:05 +0500

    fix: add missing interface

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import PlatformSelector from "./components/PlatformSelector";
 import { Genre } from "./hooks/useData";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -36,6 +37,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area={"main"}>
+        <GameHeading gameQuery={gameQuery} />
         <Flex marginBottom={5}>
           <Box marginRight={5}>
             <PlatformSelector
