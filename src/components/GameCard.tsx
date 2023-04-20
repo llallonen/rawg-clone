1e2ecb41ef22872565171564ef43a0b3404b5a5d
llallonen <llallonen@gmail.com>
2023-04-20 21:05:26 +0500

    fix: change some sizes of grid

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,13 +13,13 @@ const GameCard = ({ game }: Props) => {
     <Card max-width='300px' borderRadius={10} overflow={"hidden"}>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIcons
             platforms={game.parent_platforms.map((p) => p.platform)}
           ></PlatformIcons>
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
