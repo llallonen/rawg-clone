f073aa8a922ec9f102fa37170990869dbf273818
llallonen <llallonen@gmail.com>
2023-05-05 19:28:35 +0500

    feat: refactor game details page

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,20 +1,14 @@
 import {
-  Heading,
-  List,
-  ListItem,
-  SimpleGrid,
-  Spinner,
-  Text,
+    GridItem,
+    Heading, SimpleGrid,
+    Spinner
 } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import CriticScore from "../components/CriticScore";
-import DefinitionItem from "../components/DefinitionItem";
 import ExpandableText from "../components/ExpandableText";
 import GameDetails from "../components/GameDetails";
 import GameScreenshot from "../components/GameScreenshot";
 import GameTrailer from "../components/GameTrailer";
 import useGame from "../hooks/useGame";
-import { useGames } from "../hooks/useGames";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -26,11 +20,17 @@ const GameDetailPage = () => {
 
   return (
     <>
-      <Heading>{game?.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameDetails game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameScreenshot gameId={game.id}/>
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+        <GridItem>
+          <Heading>{game?.name}</Heading>
+          <ExpandableText>{game.description_raw}</ExpandableText>
+          <GameDetails game={game} />
+        </GridItem>
+        <GridItem>
+          <GameTrailer gameId={game.id} />
+          <GameScreenshot gameId={game.id} />
+        </GridItem>
+      </SimpleGrid>
     </>
   );
 };
